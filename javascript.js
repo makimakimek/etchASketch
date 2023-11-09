38f50f3228813a9bc37eac05b7575b071cc6b5b7
makimakimek <melodiezgi17@gmail.com>
2023-11-10 02:32:29 +0300

    added prompt button to get the number of squares for the grid & fixed the css so that it always uses the same total space as before even if the number of squares changes

--- javascript.js
+++ javascript.js
@@ -1,16 +1,16 @@
-function gridCreation() {
+function gridCreation(userAnswer) {
     const container = document.querySelector('#container');
 
-    counter = 0;
+    let counter = 0;
     
-    for(let i = 0; i < 16; i++) {
+    for(let i = 0; i < userAnswer; i++) {
         counter++;
         const groupSquareContainer = document.createElement('div');
         let containerName = `container${counter}`;
         groupSquareContainer.setAttribute('id', containerName);
         container.appendChild(groupSquareContainer);
 
-        for(let j = 0; j < 16; j++) {
+        for(let j = 0; j < userAnswer; j++) {
             const square = document.createElement('div');
             let squareName = `square${counter}`;
             square.setAttribute('id', squareName);
@@ -20,11 +20,41 @@ function gridCreation() {
                 event.target.style.backgroundColor = "black";
             });
 
-            square.addEventListener("mouseout", (event) => {
+            /*square.addEventListener("mouseout", (event) => {
                 event.target.style.backgroundColor = "white";
-            });
+            });*/
         }
     }
 }
 
-gridCreation();
+function newGrid() {
+    const gridButton = document.querySelector('#button');
+    let userAnswer = 16;
+    gridCreation(userAnswer);
+
+    gridButton.addEventListener("click", (event) => {
+        for(let i = 0; i < userAnswer; i++) {            
+            for(let j = 0; j < userAnswer; j++) {
+                let squareName = `#square${j + 1}`;
+                const square = document.querySelector(squareName);
+                square.remove();
+            }
+        }
+
+        for(let i = 0; i < userAnswer; i++) {
+            let containerName = `#container${i + 1}`;
+            const groupSquareContainer = document.querySelector(containerName);
+            groupSquareContainer.remove();
+        }
+
+        userAnswer = prompt("how many squares per side would you want for the new grid?");
+
+        while(userAnswer > 100) {
+            userAnswer = prompt("please put in a number between 0-100");
+        }
+        
+        gridCreation(userAnswer);
+    });
+}
+
+newGrid();
